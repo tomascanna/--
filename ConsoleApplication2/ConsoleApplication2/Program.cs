c6476d45bd68cd2e8a87662c86a949fe383a21ee
Tomas <tomas742011@gmail.com>
2020-09-16 13:21:18 -0300

    primer comit

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            ArrayList numeros = new ArrayList(50);
+
+            int i = 0;
+            var menor=0;
+           
+            for (i = 0; i < numeros.Count; i++)
+            {
+                numeros.Add(r.Next());
+            }
+
+            Console.WriteLine("la cantidad de elementos del arraylist es de: " + numeros.Capacity);
+            Console.ReadKey();
+          
+
+            foreach (int item in numeros)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
